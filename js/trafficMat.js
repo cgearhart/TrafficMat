4bf8d15bbd14bb9096fc811f51bcc5f327119f1b
CGearhart <cgearhart@gmail.com>
2013-01-05 00:45:12 -0500

    removed mobile theme buttons, added snap-back functionality, enhanced initial loading behavior to seek out GPS position if available, and improved geolocation marker & UI performance

--- js/trafficMat.js
+++ js/trafficMat.js
@@ -8,18 +8,12 @@
 
     var
 
-    button = function (icon, text) {
-        return {
-            icons: ($.browser.mobile) ? { primary: icon } : {}, // Disable icons on desktop - use text as default
-            text: ($.browser.mobile) ? false : true,  // Disable text on mobile browsers - use text as default
-            label: text
-        };
-    },
+    target_id,
 
     ui_buttons = {
-        lock: button("ui-icon-locked", "Lock"),
-        unlock: button("ui-icon-unlocked", "Unlock"),
-        refresh: button("ui-icon-refresh", "Refresh")
+        lock: { label: "Lock" },
+        unlock: { label: "Unlock" },
+        refresh: { label: "Refresh" }
     },
 
     monitorLockButton = function () {
@@ -35,6 +29,7 @@
                     localStorage.removeItem("lng");
                     localStorage.removeItem("zoom");
                     monitorLockButton();
+                    resetTimer();
                 });
             } else {
                 $( "#action" ).button( ui_buttons.lock )
@@ -46,49 +41,74 @@
                     localStorage.setItem("lng", map.center.lng());
                     localStorage.setItem("zoom", map.zoom);
                     monitorLockButton();
+                    resetTimer();
                 });
             }
         });
     },
 
-
     map = {
 
-        distanceBetween: google.maps.geometry.spherical.computeDistanceBetween,  // its a long name; alias it
+        distanceBetween: google.maps.geometry.spherical.computeDistanceBetween,  // its a long name - shorten it
 
         get isSaved () { return (localStorage.getItem("lat") ? true : false); },
         
-        get isStatic () {
-            if ( !!this.handle && !!this.center ) {
-                var dist = this.distanceBetween(this.handle.getCenter(), this.center);
-                if ( (this.zoom == this.handle.getZoom()) && dist < .0001 ) return true;
+        get isStatic () {  // test whether the map is at the saved zoom/center configuration
+            if ( this.handle && this.center ) {
+                var 
+                dist = this.distanceBetween(this.handle.getCenter(), this.center)/1000,  // convert to km
+                zoom = this.handle.getZoom();
+                if ( (this.zoom == this.handle.getZoom()) && dist < 5 ) return true; 
             }
             return false;
         },
 
         load: function () {
-            var lat = JSON.parse(localStorage.getItem("lat")),
+            var 
+            lat = JSON.parse(localStorage.getItem("lat")),
             lng = JSON.parse(localStorage.getItem("lng"));
+
             this.center = new google.maps.LatLng(lat,lng);
             this.zoom = JSON.parse(localStorage.getItem("zoom"));
         },
 
-        drawMap: function (target_id) {
+        drawMap: function () {
             var options = {  
                 center: this.center,
                 zoom: this.zoom,
+                maxZoom: 17,
+                minZoom: 4,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
-            this.handle = new google.maps.Map(window.document.getElementById(target_id), options);
+            if ( !this.handle ) {
+                this.handle = new google.maps.Map(window.document.getElementById(this.target_id), options);    
+            } else {
+                this.handle.panTo(this.center);
+                if ( this.zoom != this.handle.getZoom() ) {
+                    this.handle.setZoom(this.zoom);
+                }
+            }
         },
 
         drawTraffic: function () {
-            console.log("Drawing traffic layer.")
             this.trafficLayer = new google.maps.TrafficLayer();
             this.trafficLayer.setMap(this.handle);
         },
 
+        snapBack: function (timeLimit) {
+            if ( timeLimit > 0 && !this.isStatic ) {
+                $("#message").html("Recentering in " + timeLimit + "s...");
+                this.timer = window.setTimeout(function () { map.snapBack(timeLimit-1) }, 1000);  // 1 second delay
+            } else if ( timeLimit == 0 ) {
+                this.handle.setZoom(map.zoom);
+                this.handle.panTo(map.center);
+                resetTimer();
+            }
+            return false;
+        },
+
         initialize: function (target_id) {
+            this.target_id = this.target_id || target_id;
             if ( this.isSaved ) {
                 this.load();
             }
@@ -97,10 +117,25 @@
                 this.center =  new google.maps.LatLng(40, -98);
             }
             this.zoom = this.zoom || 5;
-            this.drawMap(target_id);
+            this.drawMap();
             this.drawTraffic();
-            monitorLockButton();  // Finish drawing the UI & monitor program state
+
+            google.maps.event.addListener(this.handle, "idle", idleListener);
+            google.maps.event.addListener(this.handle, "drag", resetTimer);
+            google.maps.event.addListener(this.handle, "zoom_changed", resetTimer);
+
         }
+    },
+
+    idleListener = function () {
+        resetTimer();
+        monitorLockButton();
+        if ( map.isSaved && !map.isStatic ) map.snapBack(10);
+    },
+
+    resetTimer = function () {
+        map.timer = ( !map.timer ? null : window.clearTimeout(map.timer));  // reset the timer
+        $( "#message" ).html("");
     },
 
     marker = {
@@ -121,16 +156,21 @@
 
     watchPosition = function () {  // use HTML5 geolocation to get device position - callbacks are asynchronous
         var
-
         glOptions = {
             enableHighAccuracy: true,
             timeout: 10000,  // default 10s timeout
             maximumAge: 300000  // 5 minute update cycle
         },
         locationFound = function(position) {  // called when HTML5 geolocation succeeds
-            console.log("success!")
+            console.log("Geolocation success");
             var location = new google.maps.LatLng(position.coords.latitude,
-                                                  position.coords.longitude);
+                                                      position.coords.longitude);
+            if ( !map.isSaved && !marker.handle ) {
+                map.center = location;
+                map.zoom = 11;
+                map.initialize();
+            }
+
             marker.draw(location);
         },
         locationError = function (error) {  // called when HTML5 geolocation fails
@@ -143,31 +183,29 @@
         },
         watchID = navigator.geolocation.watchPosition( locationFound,
                                                        locationError, 
-                                                       glOptions );
+                                                       glOptions);
         return watchID;  // Return the UID of the watch process; the UID is needed to kill the process
     };
-
 
     // Initialize the app
 
     // Start drawing the UI & bind refresh callback
     $(function() { $( "#refresh" ).button(ui_buttons.refresh); } ); // has to be wrapped in a jQuery anon function
+    $(function() { $( "#action" ).button(ui_buttons.unlock); } ); // has to be wrapped in a jQuery anon function
     $(function() { $( "#refresh" ).click( function (event, ui) { 
             map.drawTraffic();
         })
     });
 
-    // Initialize geolocation
+    // Initialize geolocation when app first loads - helps tolerate response delay
     if ("geolocation" in navigator) { 
         watchID = watchPosition();
     }
 
     // Need public method to initialize the app using "body onload"; otherwise map_canvas id doesn't exist & causes error
     ns.trafficMat = {
-        initialize: function (target_div) {
-
-            map.initialize(target_div);
-            
+        initialize: function (target_id) { 
+            map.initialize(target_id);
         }
     };
 
